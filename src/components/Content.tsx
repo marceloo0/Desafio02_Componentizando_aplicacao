887efded70f25631803ab8ebfd1377dc476bea75
marcelo barbosa <marceloppb@outlook.com>
2021-03-27 23:36:41 -0300

    atualizando componentes

---
+++ src/components/Content.tsx
@@ -0,0 +1,42 @@
+import { MovieCard } from './MovieCard'
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface MoviesProps {
+  movies: MovieProps[];
+  title: string;
+}
+
+export function Content({ movies, title }: MoviesProps) {
+  // Complete aqui
+  return (
+    <>
+      <header>
+        <span className="category">Categoria:<span> {title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard 
+              key ={movie.imdbID} 
+              title={movie.Title} 
+              poster={movie.Poster} 
+              runtime={movie.Runtime} 
+              rating={movie.Ratings[0].Value} 
+            />
+          ))}
+        </div>
+      </main>
+    </>
+  )
+}
